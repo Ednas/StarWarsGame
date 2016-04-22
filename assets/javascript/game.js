ea0738710a1b2ff0d666b499822a9af8ebba625b
ErnaNeal <Erna@MyCFLRealEstate.com>
2016-04-22 19:03:39 -0400

    Added variables to game.js

---
+++ assets/javascript/game.js
@@ -0,0 +1,24 @@
+//Global variables
+
+//Array of Playable Characters
+var characters = 
+var chosenCharacter =
+var chosenDefender = 
+
+//Game counters
+var hitPoints =
+var level = 
+
+//Starts the game
+function startGame() {
+
+	console.log();
+}
+
+	document.getElementById('');
+
+	document.onClick();
+
+	document.onClick()=changeColor();
+
+	
