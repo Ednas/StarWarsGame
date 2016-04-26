732a925c06e0f7eef61cfc24c173628cfb768d9e
ErnaNeal <Erna@MyCFLRealEstate.com>
2016-04-26 18:52:31 -0400

    Added some variables and functions to js

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,11 +3,42 @@
 //Array of Playable Characters
 var characters = ['ben', 'darth', 'finn', 'rey'];
 var chosenCharacter = [];
+var remainingCharacter = [];
 var chosenDefender = [];
 
-//Game counters
-var hitPoints =
-var level = 
+//stolen from stopwatch to try and plug in here
+window.onload = function(){
+  //click events are done for you:
+  $('.bena').click(characters.ben);
+  $('.dartha').click(characters.darth);
+  $('.finna').click(characters.finn);
+  $('.reya').click(characters.rey);
+};
+
+var players = 
+{
+	rey:function()
+{
+	hitpoints = 180
+	damage = 20
+}
+	darth:function()
+{
+	hitpoints = 150
+	damage = 15
+}
+	finn:function()
+{
+	hitpoints = 125
+	damage = 10
+}
+	ben:function()
+{
+	hitpoints = 100
+	damage = 8
+}
+}
+
 
 //Starts the game
 function startGame() {
@@ -18,10 +49,16 @@ function startGame() {
 
 	console.log("characters");
 }
-
+//Random elements I think I will need
+//Selects an element
 	document.getElementById('');
-
+//Does a thing when clicked
 	document.onClick();
-
+//When clicked - character background will change color
 	document.onClick()=changeColor();
 
+//Attempting to do the math to reduce the hitpoints in a fight
+fight function();{
+	var hitPoints
+	var defense
+}
